38d2136dbbc82cfc7b8b1d77442e3dc9e25e81f9
developer-anil <developeranil007@gmail.com>
2019-06-04 23:47:57 +0530

    Implement date filter
    - Filter invoices with date from and date to filters

--- app/routes/invoices.js
+++ app/routes/invoices.js
@@ -1,8 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  queryParams: {
+    dateFrom: {
+      refreshModel:true
+    },
+    dateTo: {
+      refreshModel: true
+    }
+  },
+
   model(params) {
     return this.store.query('invoice', params);
   }
 });
-
